e8a7a407acbfb4384ee998384a9ea6154d214b3e
xiesen110 <xiesen310@qq.com>
2019-07-27 14:42:18 +0800

    Initial commit

---
+++ src/main/java/top/xiesen/springboot/App.java
@@ -0,0 +1,13 @@
+package top.xiesen.springboot;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
